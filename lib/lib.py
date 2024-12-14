9e1a36be7e0a9367e7e5ad34d150c66a57305bd0
Israel Benicio <israelbenicios@gmail.com>
2024-12-14 18:54:30 -0300

    lib atualizada

--- lib/lib.py
+++ lib/lib.py
@@ -1,22 +1,27 @@
 import os, csv
 from gdown import download
 
-# Função cria um diretorio    
 def verificar_diretorio(nome_diretorio:str):
+    """
+    Criar uma pasta, se a mesma não existir no diretorio do algoritmo
+    """
     if not os.path.exists(nome_diretorio):
         os.mkdir(nome_diretorio)
     return None
 
-
-# Definir a função download_from_drive
 def download_dataset(link,filename):
+    """
+    Baixar o arquivo do Google Drive na pasta especificada
+    """
     file_id = link.split('/')[-2]
     url = f'https://drive.google.com/uc?id={file_id}'
     download(url, filename, quiet = False)
     return None
 
-#Função que 
 def preencher_matriz_contratos(nome_arquivo: str):
+    """
+    Gera e preenche a matriz tridimensional com os valores dos contratos
+    """
     matriz = []
     with open(nome_arquivo, 'r') as arquivo:
 
@@ -42,15 +47,21 @@ def preencher_matriz_contratos(nome_arquivo: str):
     return m, n, t, matriz
 
 def imprimir_matriz(matriz, k=None):
+    """
+    Apresenta a matriz gerada em tela
+    """
     if k is None:
         for i in range(len(matriz)):
-            print(" ".join(str(elemento) for elemento in matriz[i]))# Utilizei o join para melhorar um pouco a visualização da matriz
+            print(" ".join(str(elemento) for elemento in matriz[i]))
             print()
     else:
         print(matriz[k])
     return None
 
 def exportar_csv(nome_arquivo, matriz):
+    """
+    Exporta a matriz em um arquivo CSV
+    """
     with open(nome_arquivo, 'w', newline = '') as arq_csv:
         # Definindo contador inicial
         contador: int = 1
